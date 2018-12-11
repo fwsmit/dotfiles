23ba3587ed81f6c99febd0e7fd5fab97bb86cac7
fwsmit <fw.smit01@gmail.com>
2018-12-11 20:22:19 +0100

    updated install scripts

--- installPackages.sh
+++ installPackages.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-sudo pacman -Syu python gnome gvim make cmake sfml zsh zsh-completions base-devel
+sudo pacman -Syu python gnome gvim make cmake sfml zsh zsh-completions base-devel neovim python-neovim xclip
 # aur packages:
 # armadillo (oh-my-zsh-git) tgui
