97069df678f25c0c394964289d0ce09cf8815106
fwsmit <fw.smit01@gmail.com>
2020-10-05 18:19:33 +0200

    Remove picom and add thunderbird shortcut

--- private_dot_local/bin/executable_configureDisplay.sh
+++ private_dot_local/bin/executable_configureDisplay.sh
@@ -22,21 +22,23 @@ done
 
 shift $((OPTIND-1))
 
-if [ $COMPOSITOR = true ]
-then
-	# start compositor
-	picom --config ~/.config/picom/picom.conf &
-else
-	# kill compositor, this is recommended when turning on the external screen as it may cause problems otherwise
-	killall picom
-fi
+# if [ $COMPOSITOR = true ]
+# then
+	# # start compositor
+	# picom --config ~/.config/picom/picom.conf &
+# else
+	# # kill compositor, this is recommended when turning on the external screen as it may cause problems otherwise
+	# killall picom
+# fi
 
 if [ $SMALLDISPLAY = false ]
 then
-	# turn on big display
-	xrandr --output HDMI-1-0 --left-of eDP-1 --auto
-	#turn off laptop display
-	xrandr --output eDP-1 --off
+	# # turn on big display
+	# xrandr --output HDMI-1-0 --left-of eDP-1 --auto
+	# #turn off laptop display
+	# xrandr --output eDP-1 --off
+	
+	xrandr --output HDMI-1-0 --auto --output eDP-1 --off
 else
 	#turn on laptop display
 	xrandr --output eDP-1 --left-of HDMI-1-0 --auto
